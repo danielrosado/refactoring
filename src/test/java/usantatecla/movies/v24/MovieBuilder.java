fbb6f26428616c1ae86b862d6ecc3ed61620c3c0
pablocc <pablocc1996@gmail.com>
2021-04-20 20:01:56 +0200

    v24 - Adapt MovieBuilder test to new Movie class

--- src/test/java/usantatecla/movies/v24/MovieBuilder.java
+++ src/test/java/usantatecla/movies/v24/MovieBuilder.java
@@ -1,17 +1,10 @@
 package usantatecla.movies.v24;
 
-import usantatecla.movies.v23.ChildrenPrice;
-import usantatecla.movies.v23.Movie;
-import usantatecla.movies.v23.NewReleasePrice;
-import usantatecla.movies.v23.Price;
-import usantatecla.movies.v23.RegularPrice;
-
 public class MovieBuilder {
 
 	private String title;
-	
-	private Price price;
-	
+	private Movie movie;
+
 	public MovieBuilder() {
 		title = "movieName";
 	}
@@ -22,21 +15,21 @@ public class MovieBuilder {
 	}
 	
 	public MovieBuilder childrens() {
-		this.price = new ChildrenPrice();
+		this.movie = new ChildrenMovie(this.title);
 		return this;
 	}
 	
 	public MovieBuilder regular() {
-		this.price = new RegularPrice();
+		this.movie = new RegularMovie(this.title);
 		return this;
 	}
 	
 	public MovieBuilder newRelease() {
-		this.price = new NewReleasePrice();
+		this.movie = new NewReleaseMovie(this.title);
 		return this;
 	}
 	
-	public usantatecla.movies.v23.Movie build() {
-		return new Movie(title, price);
+	public Movie build() {
+		return this.movie;
 	}
 }
